2733c83ed87ed5454c0dd001be58746d9e7f43f1
Yoo Ri HWang <hwany20@wfu.edu>
2021-07-07 04:38:49 -0400

    1

--- scripts/01-scrape-page-one.R
+++ scripts/01-scrape-page-one.R
@@ -17,38 +17,72 @@ titles <- page %>%
   html_nodes(".iteminfo") %>%
   html_node("h3 a") %>%
   html_text() %>%
-  ___()
-
+  str_squish()
+titles
 # scrape links -----------------------------------------------------------------
 
 links <- page %>%
   html_nodes(".iteminfo") %>%
   html_node("h3 a") %>%
   html_attr("href") %>%
-  str_replace("\\.", "___")
+  str_replace("\\.", "https://collections.ed.ac.uk/art")
 
+links
+ ?str_replace()
 # scrape artists ---------------------------------------------------------------
 
 artists <- page %>%
   html_nodes(".iteminfo") %>%
   html_node(".artist") %>%
-  ___
+  html_text() %>%
+  str_squish()
 
 # put together in a data frame -------------------------------------------------
 
 first_ten <- tibble(
-  title = ___,
-  artist = ___,
-  link = ___
+  title = titles,
+  artist = artists,
+  link = links
 )
 
 # scrape second ten paintings --------------------------------------------------
 
-second_url <- "___"
 
-page <- read_html(second_url)
-...
+second_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=10"
 
-second_ten <- tibble(
-  ...
+# read second page --------------------------------------------------------------
+
+page2 <- read_html(second_url)
+
+# scrape titles ----------------------------------------------------------------
+
+titles2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+titles
+# scrape links -----------------------------------------------------------------
+
+links2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace("\\.", "https://collections.ed.ac.uk/art")
+
+# scrape artists ---------------------------------------------------------------
+
+artists2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node(".artist") %>%
+  html_text() %>%
+  str_squish()
+
+# put together in a data frame -------------------------------------------------
+
+sec_ten <- tibble(
+  title2 = titles2,
+  artist2 = artists2,
+  link2 = links2
 )
+
